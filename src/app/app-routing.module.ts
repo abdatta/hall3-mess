d626ca67c4ae4ce9978d52d1cd262c53c41110be
Ashish kr Singh <naksiitk@gmail.com>
2018-10-13 13:56:45 +0530

    Add home and main-nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';     // Add your component here
 import { SignupComponent } from './signup/signup.component';  // Add your component here
+import { HomeComponent } from './home/home.component';
+import { ChildActivationEnd } from '@angular/router/src/events';
+import { createViewChildren } from '@angular/compiler/src/core';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 //This is my case 
 const routes: Routes = [
@@ -16,6 +20,16 @@ const routes: Routes = [
     {
         path: 'signup',
         component: SignupComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+          {  path:'',
+            component: MainNavComponent,
+          }
+        ]
+        
     }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,36 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
