a217356f89292c4fa69f343e12b9b05e7ed0a9da
Ashish kr Singh <naksiitk@gmail.com>
2018-11-06 18:29:23 +0530

    Add snackbar to signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {MatSnackBar} from '@angular/material';
 import { AuthService } from '@app/services';
 
 @Component({
@@ -12,26 +12,35 @@ export class SignupComponent implements OnInit {
 
   submitted = false;
   error = '';
-
+  noinput = '';
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router , public snackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
 
+  openSnackBar() {
+    this.snackBar.open( this.error , this.noinput , {
+      duration: 2700,
+    });
+  }
   signUp(rollno: string, password: string, repassword: string) {
     if (password === repassword) {
       this.authService.signUp(rollno, password)
         .subscribe(s => {
           if (s === 200) {
             this.router.navigateByUrl('/home');
+            this.error = 'Please Check your mail for first time PIN';
+            this.openSnackBar();
           } else {
             this.error = 'User already exists';
             this.submitted = false;
+            this.openSnackBar();
           }
         });
       } else {
         this.error = 'Passwords do not match';
+        this.openSnackBar();
       }
   }
 
