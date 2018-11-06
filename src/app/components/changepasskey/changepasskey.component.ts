b3578214be00039b8d446808fa41c9aa1d2bfdb3
Ashish kr Singh <naksiitk@gmail.com>
2018-11-06 16:12:54 +0530

    Implement form reset after success

--- src/app/components/changepasskey/changepasskey.component.ts
+++ src/app/components/changepasskey/changepasskey.component.ts
@@ -12,6 +12,10 @@ export class ChangepasskeyComponent implements OnInit {
 
   error = '';
   noinput = '';
+  input1: string;
+  input2: string;
+  input3: string;
+
   constructor(private authService: AuthService,
     private router: Router , public snackBar: MatSnackBar) { }
 
@@ -24,22 +28,34 @@ export class ChangepasskeyComponent implements OnInit {
     });
   }
 
+  clear() {
+    this.input1 = '';
+    this.input2 = '';
+    this.input3 = '';
+  }
+
   chngpin(oldpin: string, newpin: string , renewpin: string ) {
     if (oldpin === newpin) {
        this.error = 'New PIN isnt New'; this.openSnackBar();
+       this.input2 = '';
+       this.input3 = '';
       } else if (newpin === renewpin) {
     this.authService.chngpin(oldpin , newpin)
         .subscribe((s: number) => {
       if (s === 200) {
         this.error = 'PIN successfully changed';
         this.openSnackBar();
+        this.clear();
       } else if (s === 403) {
         this.error = 'current PIN is wrong';
         this.openSnackBar();
+        this.clear();
       } else {
         this.error = 'Some error occured, Please try again';
         this.openSnackBar();
+        this.clear();
       }
-    }); } else { this.error = 'Retype PIN correctly'; this.openSnackBar(); }
+    }); } else { this.error = 'Retype PIN correctly'; this.openSnackBar(); this.input2 = '';
+    this.input3 = ''; }
   }
 }
