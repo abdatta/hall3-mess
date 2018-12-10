4e8725d14b8c37d96a483e5dbb0b66a5fbb1c9ee
Ashish kr Singh <naksiitk@gmail.com>
2018-12-10 23:17:15 +0530

    Add recents to mess

--- src/app/modules/shared/main-nav/main-nav.module.ts
+++ src/app/modules/shared/main-nav/main-nav.module.ts
@@ -5,14 +5,17 @@ import { MaterialModule } from '@shared/material';
 
 import { MainNavComponent } from '@shared/main-nav/main-nav.component';
 
+import { RecentsComponent } from '@shared/main-nav/recents/recents.component';
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    MaterialModule
+    MaterialModule,
   ],
   declarations: [
-    MainNavComponent
+    MainNavComponent,
+    RecentsComponent
   ],
   exports: [
     MainNavComponent
---
+++ src/app/modules/shared/main-nav/recents/recents.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recents',
+  templateUrl: './recents.component.html',
+  styleUrls: ['./recents.component.css']
+})
+export class RecentsComponent implements OnInit {
+
+  rollno = 180161;
+  loginactivity = [
+    {
+      detail: { rollno: 180537, name: 'Arjun'},
+      time: '1 min ago',
+      order: '',
+    },
+    {
+      detail: { rollno: 170161, name: 'Ashish'},
+      time: '1 min ago',
+      order: '',
+    },
+    {
+      detail: { rollno: 180360, name: 'Tarun Book'},
+      time: '2 min ago',
+      order: '',
+    },
+    {
+      detail: { rollno: 180198, name: 'Jolly Llb'},
+      time: '2 min ago',
+      order: 'New dish Chicken Soup will be available every Wednesday in Dinner. It is Prebookable.',
+    },
+    {
+      detail: { rollno: 180456, name: 'Praveen'},
+      time: '2 min ago',
+      order: 'Pay your mess bill at this link: ',
+    },
+    {
+      detail: { rollno: 180432, name: 'Rajan'},
+      time: '3 min ago',
+      order: 'New dish Fish Curry will be available every Saturday in Dinner. It is Prebookable.',
+    },
+    {
+      detail: { rollno: 180345, name: 'Rahul'},
+      time: '3 min ago',
+      order: 'Kindly give your valuable feedback using the feedback form link: ',
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
