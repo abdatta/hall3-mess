c3f8af825005dcf75baa1e756091b98b7c0baa1f
abdatta <abdatta@iitk.ac.in>
2018-12-21 18:52:44 +0530

    Add an update available message when the PWA has an update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hall 3 Mess Automation';
+
+  update = false;
+
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      this.update = true;
+    });
+  }
 }
