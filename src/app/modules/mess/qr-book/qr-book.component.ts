3b6ac2916ee0558ae81d741720913e5b13d79657
Abhishek Datta <abdatta@iitk.ac.in>
2019-01-06 00:19:00 +0530

    Remove console logs from qr component

--- src/app/modules/mess/qr-book/qr-book.component.ts
+++ src/app/modules/mess/qr-book/qr-book.component.ts
@@ -29,10 +29,10 @@ export class QRBookComponent implements OnInit {
     this.loading = true;
     this.dishesService.getSomedaysDishes(moment().format('dddd'))
         .subscribe(dishes => {
-          const data_ids = data.map(d => d.slice(0, 2)); console.log(data_ids);
+          const data_ids = data.map(d => d.slice(0, 2));
           const data_qty = data.map(d => +d.slice(2));
 
-          this.dishes = dishes.filter(dish => {console.log(dish.name, dish.short_id);
+          this.dishes = dishes.filter(dish => {
             if (data_ids.includes(dish.short_id)) {
               dish.quantity = data_qty[data_ids.indexOf(dish.short_id)];
               return true;
@@ -58,7 +58,6 @@ export class QRBookComponent implements OnInit {
             this.tokenService.bookToken(this.dishes)
                 .subscribe(token => {
                   this.snackBar.open('Dishes booked successfully');
-                  console.log(token);
                   this.dialogRef.close();
                 });
           } else if (s === 401) {
