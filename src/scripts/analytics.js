91c8c870cea71b5c23ab10bd8e6c8fe894e43fd2
Abhishek Datta <abdatta@iitk.ac.in>
2019-01-10 19:30:46 +0530

    Disable analytics in development

--- src/scripts/analytics.js
+++ src/scripts/analytics.js
@@ -1,4 +1,4 @@
-if (window.location.hostname === 'dev.mess.hall3iitk.com') {
+if (false) {
     (function(i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
         i[r] = i[r] || function() {
